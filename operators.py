adb698bd5e7005bd13785d760cb15a07ba8634ff
Aditi Mahadar <92514624+adimsanjay@users.noreply.github.com>
2024-02-06 18:49:47 +0530

    operators in python

---
+++ operators.py
@@ -0,0 +1,57 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#-----------operators------------
+#----------Arithmetic Operator----------
+10+20
+30
+10-20
+-10
+20*2
+40
+40/2
+20.0
+40//2
+20
+40%10
+0
+20**2
+400
+KeyboardInterrupt
+#--------comparison operator----------
+4>2
+True
+4<6
+True
+4<=6
+True
+7>=5
+True
+7!=5
+True
+'Aditi'=='aditi'
+False
+'Aditi'=='Aditi'
+True
+3==3
+True
+#--------logical operator--------
+#and or not
+fruit='apple'
+fruit
+'apple'
+place='kashmir'
+place
+'kashmir'
+fruit=='apple' and place=='pune'
+False
+fruit=='apple' and place=='kashmir'
+True
+fruit=='apple' or place=='pune'
+True
+
+not fruit=='mango'
+True
+not place='kashmir'
+SyntaxError: cannot assign to expression
+not place=='kashmir'
+False
